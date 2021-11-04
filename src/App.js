a755edb49d54d30c46f9b645ee27999e3d7af8c8
Kartik Mehta <kartikmehta202@gmail.com>
2021-11-04 14:02:04 +0530

    Language +3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import AceEditor from "react-ace";
 
 function App() {
     const [code, setCode] = useState("");
     const [output, setOutput] = useState("");
+    const [language, setLanguage] = useState("cpp");
 
     const handleSubmit = async () => {
         console.log(code);
 
         const payload = {
-            language: "cpp",
+            language: language,
             code,
         };
 
@@ -19,22 +21,57 @@ function App() {
                 payload
             );
             setOutput(data.output);
-        } catch (err) {
-            console.log(err.response);
+        } catch ({ response }) {
+            if (response) {
+                const errMsg = response.data.err.stderr;
+                setOutput(errMsg);
+            } else {
+                setOutput("Error connecting to server!");
+            }
         }
     };
 
     return (
-        <div className="">
+        <div className="" style={{ backgroundColor: "lightgray" }}>
             <h1>ONLINE CODE COMPILER</h1>
-            <textarea
-                rows="20"
-                cols="75"
+            <div>
+                <select
+                    value={language}
+                    onChange={(e) => {
+                        setLanguage(e.target.value);
+                    }}
+                >
+                    <label>Language : </label>
+                    <option value="c">C</option>
+                    <option value="cpp">C++</option>
+                    <option value="py">Python</option>
+                    <option value="js">Javascript</option>
+                </select>
+                <br />
+                <br />
+            </div>
+            <br />
+
+            <AceEditor
+                mode="javascript"
+                theme="github"
                 value={code}
                 onChange={(e) => {
-                    setCode(e.target.value);
+                    setCode(e);
                 }}
-            ></textarea>
+                fontSize={16}
+                showPrintMargin={true}
+                showGutter={true}
+                highlightActiveLine={true}
+                setOptions={{
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                    showLineNumbers: true,
+                    tabSize: 4,
+                }}
+            />
+
             <br />
             <button onClick={handleSubmit}>Submit</button>
             <p>{output}</p>

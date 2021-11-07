0ab8fed910b1b5f78aedde3e556699476e9e5638
Prerna Choudhary <choudharyprerna493@gmail.com>
2021-11-07 11:14:27 +0530

    UI done.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,44 @@ function App() {
     const [output, setOutput] = useState("");
     const [language, setLanguage] = useState("cpp");
 
+    const myStyle = {
+        color: "white",
+        backgroundColor: "#181D31",
+        // padding: "10px",
+        fontFamily: "Cursive",
+        borderRadius: '20px'
+        // display: "flex",
+    };
+
+    const myStyle1 = {
+        color: "#181D31",
+        backgroundColor: "#A8E6CF",
+        padding: "10px",
+        fontFamily: "San-sarif",
+        margin: "auto"
+    };
+    const myStyle2 = {
+        borderRadius: '20px',
+        color: "white",
+        backgroundColor: "#181D31",
+        fontSize: "1.5rem",
+        padding: "20px",
+        fontFamily: "San-sarif",
+        marginLeft: "100px",
+        margin: "auto",
+        border: "solid",
+        height: "70vh",
+        width: "40%"
+    };
+    const dflex = {
+        display: "flex",
+    }
     const handleSubmit = async () => {
         console.log(code);
-
         const payload = {
             language: language,
             code,
         };
-
         try {
             const { data } = await axios.post(
                 "http://localhost:5000/run",
@@ -32,49 +62,62 @@ function App() {
     };
 
     return (
-        <div className="" style={{ backgroundColor: "lightgray" }}>
-            <h1>ONLINE CODE COMPILER</h1>
-            <div>
-                <select
-                    value={language}
-                    onChange={(e) => {
-                        setLanguage(e.target.value);
-                    }}
-                >
-                    <label>Language : </label>
-                    <option value="c">C</option>
-                    <option value="cpp">C++</option>
-                    <option value="py">Python</option>
-                    <option value="js">Javascript</option>
-                </select>
-                <br />
-                <br />
+        <div className="" style={myStyle1} >
+            <h1 style={{ marginLeft: "30%" }}>ONLINE CODE COMPILER</h1>
+            <div style={dflex}>
+                <div >
+                    <div >
+                        <select
+                        style={myStyle}
+                            // style={{  }}
+                            value={language}
+                            onChange={(e) => {
+                                setLanguage(e.target.value);
+                            }}
+                        >
+                            <label >Language : </label>
+                            <option value="c">C</option>
+                            <option value="cpp">C++</option>
+                            <option value="py">Python</option>
+                            <option value="js">Javascript</option>
+                        </select>
+                        <br />
+                        <br />
+                    </div>
+
+                    {/* <br /> */}
+
+                    <AceEditor
+
+                        mode="javascript"
+                        theme="google"
+                        value={code}
+                        height='75vh'
+                        width='650px'
+                        // wrapEnabled="true"
+                        style={myStyle}
+                        onChange={(e) => {
+                            setCode(e);
+                        }}
+
+                        fontSize={20}
+                        showPrintMargin={true}
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        setOptions={{
+                            enableBasicAutocompletion: true,
+                            enableLiveAutocompletion: true,
+                            enableSnippets: true,
+                            showLineNumbers: true,
+                            tabSize: 100,
+                        }}
+                    />
+
+                    <br />
+                    <button onClick={handleSubmit} style={myStyle}>Submit</button>
+                </div>
+                <div style={myStyle2}>{output}</div>
             </div>
-            <br />
-
-            <AceEditor
-                mode="javascript"
-                theme="github"
-                value={code}
-                onChange={(e) => {
-                    setCode(e);
-                }}
-                fontSize={16}
-                showPrintMargin={true}
-                showGutter={true}
-                highlightActiveLine={true}
-                setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: true,
-                    showLineNumbers: true,
-                    tabSize: 4,
-                }}
-            />
-
-            <br />
-            <button onClick={handleSubmit}>Submit</button>
-            <p>{output}</p>
         </div>
     );
 }
